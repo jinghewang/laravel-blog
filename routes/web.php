0b1b8831979ae89d98cd07fc66828f5c2c1f4a8c
jinghewang <jinghewang@163.com>
2017-07-20 10:07:06 +0800

    student2

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::resource('home','HomeController');
+Auth::routes();
 
 Route::resource('students', 'StudentController');
+
+Route::get('/home', 'HomeController@index')->name('home');
